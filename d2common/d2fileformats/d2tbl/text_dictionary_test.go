5d5733f2fce4628ffee36075b39155b8376b85f2
M. Sz <mszeptuch@protonmail.com>
2021-03-04 09:11:35 +0100

    text dictionary: wip: added test for data file

--- d2common/d2fileformats/d2tbl/text_dictionary_test.go
+++ d2common/d2fileformats/d2tbl/text_dictionary_test.go
@@ -1,21 +1,47 @@
 package d2tbl
 
 import (
+	"os"
+
 	"testing"
 )
 
-func exampleData() *TextDictionary {
-	result := &TextDictionary{
-		"abc":        "def",
-		"someStr":    "Some long string",
-		"teststring": "TeStxwsas123 long strin122*8:wq",
+func exampleData() TextDictionary {
+	result := TextDictionary{
+		"abc":             "def",
+		"someStr":         "Some long string",
+		"teststring":      "TeStxwsas123 long strin122*8:wq",
+		"multilinestring": "this is come string\nwith more then one line\n123",
 	}
 
 	return result
 }
 
-func TestTBL_Marshal(t *testing.T) {
-	tbl := exampleData()
+func TestTBL_Marshal_testdata(t *testing.T) {
+	testFile, fileErr := os.Open("testdata/testdata.tbl")
+	if fileErr != nil {
+		t.Error("cannot open test data file")
+		return
+	}
+
+	fileData := make([]byte, 0)
+	buf := make([]byte, 16)
+
+	for {
+		numRead, err := testFile.Read(buf)
+
+		fileData = append(fileData, buf[:numRead]...)
+
+		if err != nil {
+			break
+		}
+	}
+
+	tbl, err := LoadTextDictionary(fileData)
+	if err != nil {
+		t.Error(err)
+	}
+
 	data := tbl.Marshal()
 
 	newTbl, err := LoadTextDictionary(data)
@@ -23,7 +49,30 @@ func TestTBL_Marshal(t *testing.T) {
 		t.Error(err)
 	}
 
-	for key, value := range *tbl {
+	for key, value := range tbl {
+		newValue, ok := newTbl[key]
+
+		if !ok {
+			t.Fatalf("string %s wasn't encoded to table", key)
+		}
+
+		if newValue != value {
+			t.Fatal("unexpected value set")
+		}
+	}
+}
+
+func TestTBL_Marshal(t *testing.T) {
+	tbl := exampleData()
+
+	data := tbl.Marshal()
+
+	newTbl, err := LoadTextDictionary(data)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for key, value := range tbl {
 		newValue, ok := newTbl[key]
 
 		if !ok {
